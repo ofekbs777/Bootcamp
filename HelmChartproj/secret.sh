8c623ec4f8909726e3ea2ff4f4f96f30fea1e6dd
ofekbs777 <108810558+ofekbs777@users.noreply.github.com>
2022-08-03 15:57:24 +0300

    Delete HelmChartproj directory

--- HelmChartproj/secret.sh
+++
@@ -1,5 +0,0 @@
-
-kubectl create secret docker-registry ofekproj \
-  --docker-server=${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com \
-  --docker-username=AWS \
-  --docker-password=$(aws ecr get-login-password)
